e630a7a36251aebc5c4eafbfe365d9d9673b666b
arpitvadariya009 <arpitvadariya009@gmail.com>
2025-10-23 17:22:51 +0530

    color code added

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const categprySchema = new mongoose.Schema({
     name: String,
     image: String,
+    colorCode : String,
     status: {
         type: Boolean,
         default: true
